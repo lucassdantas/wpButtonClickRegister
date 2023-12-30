d0f0a40c294175280ca572182a421ac31785afaa
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-12-29 23:35:14 -0300

    builiding ajax request

--- wpButtonClickRegister.php
+++ wpButtonClickRegister.php
@@ -8,13 +8,68 @@
  * Author URI:  linkedin.com/in/lucas-de-sousa-dantas/
  */
 
- if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+ if ( ! defined( 'ABSPATH' ) ||  !function_exists('add_action')) {
+	exit;
 }
-if(!function_exists('add_action')){
-    die;
+
+
+register_activation_hook( __FILE__, 'create_table_and_shortcode' );
+function create_table_and_shortcode() {
+    create_clicks_register_table();
+
+    add_shortcode('lc_button_click_register', 'button_click_register');
 }
-function button_click_register(){
+function create_clicks_register_table() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'lc_button_clicks';
 	
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE IF NOT EXISTS $table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		click_time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+		PRIMARY KEY  (id)
+	) $charset_collate;";
+
+	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+	dbDelta( $sql );
+    
 }
+
+
 add_shortcode('lc_button_click_register', 'button_click_register');
+function button_click_register(){
+    echo "<a class='lc_button_click_register'>Botão de registro</a>";
+}
+
+add_action('wp_footer', 'button_click_script');
+function button_click_script(){
+?>
+    <script defer>
+        let allLcButtons = document.querySelectorAll('.lc_button_click_register') || undefined
+        if(allLcButtons){
+            allLcButtons.forEach(button => {
+                button.addEventListener('click',() => {
+                    jQuery.ajax({
+                        url: "<?php echo admin_url('admin-ajax.php'); ?>",
+                        type: "POST",
+                        cache: false,
+                        data:{ 
+                            action: 'send_email', 
+                            clickMoment: moment(),
+                        },
+                        success:function(res){}
+                    }); 
+                })
+            })
+        }
+    </script>
+<?php
+}
+
+add_action( 'wp_ajax_register_click', 'register_click' );
+add_action( 'wp_ajax_nopriv_register_click', 'register_click' );
+
+function register_click(){
+    
+}
